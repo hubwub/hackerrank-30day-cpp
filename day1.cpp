78412b25b7547c1f8a20c63ba7b4f074b8d15584
Anne Celestino <hubwub@tagspace.me>
2016-05-25 19:12:14 -0500

    solved Day 1

---
+++ day1.cpp
@@ -0,0 +1,27 @@
+#include <iomanip>
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main() {
+  int i = 4;
+  double d = 4.0;
+  string s = "HackerRank ";
+  // Declare second integer, double, and String variables.
+  int a;
+  double b;
+  string c;
+  // Read and save an integer, double, and String to your variables.
+  cin >> a >> b;
+  cin.ignore();
+  getline(cin, c);
+  // Print the sum of both integer variables on a new line.
+  cout << i + a << endl;
+  // Print the sum of the double variables on a new line.
+  cout << setiosflags(ios::fixed) << setprecision(1) << d + b << endl;
+  // Concatenate and print the String variables on a new line
+  // The 's' variable above should be printed first.
+  cout << s + c << endl;
+  return 0;
+}
