9e629ccb3705353a4e7e0f96d847de0b9fd943f4
Anne Celestino <hubwub@tagspace.me>
2016-05-29 06:24:26 -0500

    catching up on missed days

---
+++ day5.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  for (int i = 1; i <= 10; i++) {
+    cout << N << " x " << i << " = " << (N * i) << endl;
+  }
+
+  return 0;
+}
